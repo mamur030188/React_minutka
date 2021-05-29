2810f977e1b14fc3d1f995ee55951a89fe9db32e
Mamirjon Mamadaliev <mike030188@gmail.com>
2021-05-29 16:21:53 +0900

    Components Part 2

---
+++ src/shared/dishes.js
@@ -0,0 +1,186 @@
+export const DISHES =
+    [
+        {
+        id: 0,
+        name:'Dodapizza',
+        image: 'assets/images/dodapizza.png',
+        category: 'mains',
+        label:'Hot',
+        price:'8.990',
+        description:'A unique combination of Uzbek Doda (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Kavkaz onion, Indoneziyan red chillies and Fresh cheese.',
+        comments: [
+            {
+            id: 0,
+            rating: 5,
+            comment: "Imagine all the eatables, living in minutka!",
+            author: "Jasur Lochinov",
+            date: "2014-10-16T17:57:28.556094Z"
+            },
+            {
+            id: 1,
+            rating: 4,
+            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+            author: "Polat Mamatov",
+            date: "2014-09-05T17:57:28.556094Z"
+            },
+            {
+            id: 2,
+            rating: 3,
+            comment: "Eat it, just eat it!",
+            author: "Mansur Jaavlonov",
+            date: "2015-02-13T17:57:28.556094Z"
+            },
+            {
+            id: 3,
+            rating: 4,
+            comment: "Ultimate, Reaching for the stars!",
+            author: "Rashid Soliev",
+            date: "2015-12-02T17:57:28.556094Z"
+            },
+            {
+            id: 4,
+            rating: 2,
+            comment: "It's your birthday, we're gonna party!",
+            author: "25 Cent",
+            date: "2016-12-02T17:57:28.556094Z"
+            }
+        ]                        },
+        {
+        id: 1,
+        name:'Zucchipakoda',
+        image: 'assets/images/zucchipakoda.png',
+        category: 'appetizer',
+        label:'',
+        price:'2.990',
+        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+        comments: [
+            {
+            id: 0,
+            rating: 5,
+            comment: "Imagine all the eatables, living in minutka!",
+            author: "Jasur Lochinov",
+            date: "2014-10-16T17:57:28.556094Z"
+            },
+            {
+            id: 1,
+            rating: 4,
+            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+            author: "Polat Mamatov",
+            date: "2014-09-05T17:57:28.556094Z"
+            },
+            {
+            id: 2,
+            rating: 3,
+            comment: "Eat it, just eat it!",
+            author: "Mansur Jaavlonov",
+            date: "2015-02-13T17:57:28.556094Z"
+            },
+            {
+            id: 3,
+            rating: 4,
+            comment: "Ultimate, Reaching for the stars!",
+            author: "Rashid Soliev",
+            date: "2015-12-02T17:57:28.556094Z"
+            },
+            {
+            id: 4,
+            rating: 2,
+            comment: "It's your birthday, we're gonna party!",
+            author: "25 Cent",
+            date: "2016-12-02T17:57:28.556094Z"
+            }
+        ]
+        },
+        {
+        id: 2,
+        name:'Vadonut',
+        image: 'assets/images/vadonut.png',
+        category: 'appetizer',
+        label:'New',
+        price:'2.990',
+        description:'A quintessential minutka experience, is it a vada or is it a donut?',
+        comments: [
+            {
+            id: 0,
+            rating: 5,
+            comment: "Imagine all the eatables, living in minutka!",
+            author: "Jasur Lochinov",
+            date: "2014-10-16T17:57:28.556094Z"
+            },
+            {
+            id: 1,
+            rating: 4,
+            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+            author: "Polat Mamatov",
+            date: "2014-09-05T17:57:28.556094Z"
+            },
+            {
+            id: 2,
+            rating: 3,
+            comment: "Eat it, just eat it!",
+            author: "Mansur Jaavlonov",
+            date: "2015-02-13T17:57:28.556094Z"
+            },
+            {
+            id: 3,
+            rating: 4,
+            comment: "Ultimate, Reaching for the stars!",
+            author: "Rashid Soliev",
+            date: "2015-12-02T17:57:28.556094Z"
+            },
+            {
+            id: 4,
+            rating: 2,
+            comment: "It's your birthday, we're gonna party!",
+            author: "25 Cent",
+            date: "2016-12-02T17:57:28.556094Z"
+            }
+        ]
+        },
+        {
+        id: 3,
+        name:'ElaiCheese Cake',
+        image: 'assets/images/elaicheesecake.png',
+        category: 'dessert',
+        label:'',
+        price:'3.990',
+        description:'A delectable, semi-sweet Tashkent Style Cheese Cake, with Guliston cracker crust and spiced with Uzbek cardamoms',
+        comments: [
+            {
+            id: 0,
+            rating: 5,
+            comment: "Imagine all the eatables, living in minutka!",
+            author: "Jasur Lochinov",
+            date: "2014-10-16T17:57:28.556094Z"
+            },
+            {
+            id: 1,
+            rating: 4,
+            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+            author: "Polat Mamatov",
+            date: "2014-09-05T17:57:28.556094Z"
+            },
+            {
+            id: 2,
+            rating: 3,
+            comment: "Eat it, just eat it!",
+            author: "Mansur Jaavlonov",
+            date: "2015-02-13T17:57:28.556094Z"
+            },
+            {
+            id: 3,
+            rating: 4,
+            comment: "Ultimate, Reaching for the stars!",
+            author: "Rashid Soliev",
+            date: "2015-12-02T17:57:28.556094Z"
+            },
+            {
+            id: 4,
+            rating: 2,
+            comment: "It's your birthday, we're gonna party!",
+            author: "25 Cent",
+            date: "2016-12-02T17:57:28.556094Z"
+            }
+        ]
+        }
+    ];
