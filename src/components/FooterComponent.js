2bf2032c862e1c122a16e88b37bdcd3350a23a99
Mamirjon Mamadaliev <mike030188@gmail.com>
2021-05-30 11:20:55 +0900

    React Router

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavbarBrand } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function Footer(props) {
     return(
@@ -9,10 +9,10 @@ function Footer(props) {
                     <div className="col-4 offset-1 col-sm-2">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
-                            <li><a href="#" className="text-white">Home</a></li>
-                            <li><a href="#" className="text-white">About</a></li>
-                            <li><a href="#" className="text-white">Menu</a></li>
-                            <li><a href="contactus.html" className="text-white">Contact</a></li>
+                            <li><Link to="/home" className="text-white">Home</Link></li>
+                            <li><Link to="/aboutus" className="text-white">About Us</Link></li>
+                            <li><Link to="/menu" className="text-white">Menu</Link></li>
+                            <li><Link to="/contactus" className="text-white">Contact Us</Link></li>
                         </ul>
                     </div>
                     <div className="col-7 col-sm-5">
@@ -44,7 +44,7 @@ function Footer(props) {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Footer;
