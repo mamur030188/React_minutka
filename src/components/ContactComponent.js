e4873c6662fa897350674314ad75bd7f1e33b8b8
Mamirjon Mamadaliev <mike030188@gmail.com>
2021-05-30 12:34:18 +0900

    Single Page Applications Part 1

---
+++ src/components/ContactComponent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Contact(props) {
+    return(
+        <div className="container">
+            <div className="row row-content">
+                <div className="col-12">
+                <h3>Location Information</h3>
+                </div>
+                <div className="col-12 col-sm-4 offset-sm-1">
+                        <h5>Our Address</h5>
+                        <address>
+                        20, Leninsk street<br />
+                        Asaka city, Andijan<br />
+                        UZBEKISTAN<br />
+                        <i className="fa fa-phone"></i>: +998 97 332 3636<br />
+                        <i className="fa fa-fax"></i>: +998 74 332 3636<br />
+                        <i className="fa fa-envelope"></i>: <a href="mailto:minutka3636@mail.ru">minutka3636@mail.ru</a>
+                        </address>
+                </div>
+                <div className="col-12 col-sm-6 offset-sm-1">
+                    <h5>Map of our Location</h5>
+                </div>
+                <div className="col-12 col-sm-11 offset-sm-1">
+                    <div className="btn-group" role="group">
+                        <a role="button" className="btn btn-primary" href="tel:+998973323636"><i className="fa fa-phone"></i> Call</a>
+                        <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
+                        <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -7,44 +7,9 @@ export const DISHES =
         category: 'mains',
         label:'Hot',
         price:'8.990',
+        featured: true,
         description:'A unique combination of Uzbek Doda (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Kavkaz onion, Indoneziyan red chillies and Fresh cheese.',
-        comments: [
-            {
-            id: 0,
-            rating: 5,
-            comment: "Imagine all the eatables, living in minutka!",
-            author: "Jasur Lochinov",
-            date: "2014-10-16T17:57:28.556094Z"
-            },
-            {
-            id: 1,
-            rating: 4,
-            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-            author: "Polat Mamatov",
-            date: "2014-09-05T17:57:28.556094Z"
-            },
-            {
-            id: 2,
-            rating: 3,
-            comment: "Eat it, just eat it!",
-            author: "Mansur Jaavlonov",
-            date: "2015-02-13T17:57:28.556094Z"
-            },
-            {
-            id: 3,
-            rating: 4,
-            comment: "Ultimate, Reaching for the stars!",
-            author: "Rashid Soliev",
-            date: "2015-12-02T17:57:28.556094Z"
-            },
-            {
-            id: 4,
-            rating: 2,
-            comment: "It's your birthday, we're gonna party!",
-            author: "25 Cent",
-            date: "2016-12-02T17:57:28.556094Z"
-            }
-        ]                        },
+        },    
         {
         id: 1,
         name:'Zucchipakoda',
@@ -52,44 +17,8 @@ export const DISHES =
         category: 'appetizer',
         label:'',
         price:'2.990',
+        featured: false,
         description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-        comments: [
-            {
-            id: 0,
-            rating: 5,
-            comment: "Imagine all the eatables, living in minutka!",
-            author: "Jasur Lochinov",
-            date: "2014-10-16T17:57:28.556094Z"
-            },
-            {
-            id: 1,
-            rating: 4,
-            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-            author: "Polat Mamatov",
-            date: "2014-09-05T17:57:28.556094Z"
-            },
-            {
-            id: 2,
-            rating: 3,
-            comment: "Eat it, just eat it!",
-            author: "Mansur Jaavlonov",
-            date: "2015-02-13T17:57:28.556094Z"
-            },
-            {
-            id: 3,
-            rating: 4,
-            comment: "Ultimate, Reaching for the stars!",
-            author: "Rashid Soliev",
-            date: "2015-12-02T17:57:28.556094Z"
-            },
-            {
-            id: 4,
-            rating: 2,
-            comment: "It's your birthday, we're gonna party!",
-            author: "25 Cent",
-            date: "2016-12-02T17:57:28.556094Z"
-            }
-        ]
         },
         {
         id: 2,
@@ -98,44 +27,8 @@ export const DISHES =
         category: 'appetizer',
         label:'New',
         price:'2.990',
+        featured: false,
         description:'A quintessential minutka experience, is it a vada or is it a donut?',
-        comments: [
-            {
-            id: 0,
-            rating: 5,
-            comment: "Imagine all the eatables, living in minutka!",
-            author: "Jasur Lochinov",
-            date: "2014-10-16T17:57:28.556094Z"
-            },
-            {
-            id: 1,
-            rating: 4,
-            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-            author: "Polat Mamatov",
-            date: "2014-09-05T17:57:28.556094Z"
-            },
-            {
-            id: 2,
-            rating: 3,
-            comment: "Eat it, just eat it!",
-            author: "Mansur Jaavlonov",
-            date: "2015-02-13T17:57:28.556094Z"
-            },
-            {
-            id: 3,
-            rating: 4,
-            comment: "Ultimate, Reaching for the stars!",
-            author: "Rashid Soliev",
-            date: "2015-12-02T17:57:28.556094Z"
-            },
-            {
-            id: 4,
-            rating: 2,
-            comment: "It's your birthday, we're gonna party!",
-            author: "25 Cent",
-            date: "2016-12-02T17:57:28.556094Z"
-            }
-        ]
         },
         {
         id: 3,
@@ -144,43 +37,7 @@ export const DISHES =
         category: 'dessert',
         label:'',
         price:'3.990',
+        featured: false,
         description:'A delectable, semi-sweet Tashkent Style Cheese Cake, with Guliston cracker crust and spiced with Uzbek cardamoms',
-        comments: [
-            {
-            id: 0,
-            rating: 5,
-            comment: "Imagine all the eatables, living in minutka!",
-            author: "Jasur Lochinov",
-            date: "2014-10-16T17:57:28.556094Z"
-            },
-            {
-            id: 1,
-            rating: 4,
-            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-            author: "Polat Mamatov",
-            date: "2014-09-05T17:57:28.556094Z"
-            },
-            {
-            id: 2,
-            rating: 3,
-            comment: "Eat it, just eat it!",
-            author: "Mansur Jaavlonov",
-            date: "2015-02-13T17:57:28.556094Z"
-            },
-            {
-            id: 3,
-            rating: 4,
-            comment: "Ultimate, Reaching for the stars!",
-            author: "Rashid Soliev",
-            date: "2015-12-02T17:57:28.556094Z"
-            },
-            {
-            id: 4,
-            rating: 2,
-            comment: "It's your birthday, we're gonna party!",
-            author: "25 Cent",
-            date: "2016-12-02T17:57:28.556094Z"
-            }
-        ]
         }
     ];
