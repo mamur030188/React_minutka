e58c5e5b44cdef45c94bb10f2e2a9aa4db7fb388
Mamirjon Mamadaliev <mike030188@gmail.com>
2021-06-16 22:23:37 +0900

    Last update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Main from './components/MainComponent';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
